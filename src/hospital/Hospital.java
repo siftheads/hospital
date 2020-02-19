20aaf44686c8e5b7402e4dfc52c4b80f2ff96b5b
shiftaheads <pietropietru7@gmail.com>
2020-02-19 10:30:11 -0300

    aaaa

---
+++ src/hospital/Hospital.java
@@ -0,0 +1,70 @@
+
+package hospital;
+
+
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+
+
+public class Hospital extends Funcionario {
+    private int IdHospital;
+    private String Endereco;
+    ArrayList<String> funcionario = new ArrayList<String> ();
+    
+     
+    
+    public void setIdHospital(int IdHospital){
+        if (!Objects.isNull(IdHospital)){
+            this.IdHospital = IdHospital;
+            
+        }
+    }    
+    
+    
+    
+        public int getIdHospital(){
+        return this.IdHospital;
+        
+        }  
+    
+        
+        
+    public void setEndereco(String Endereco){
+        if (!"".equals(Endereco)){
+           this.Endereco = Endereco; 
+           
+        }
+    }    
+    
+    
+    
+        public String getEndereco(){
+        return this.Endereco;
+        
+    } 
+        
+    
+    
+    
+    
+   
+        
+        
+        
+        
+        
+        
+        
+        
+        
+         public Hospital( int idHospital){
+            
+            this.IdHospital = IdHospital;
+        
+        }
+
+                    
+            
+}
---
+++ src/hospital/ProntoSocorro.java
@@ -0,0 +1,72 @@
+
+package hospital;
+
+import java.util.Objects;
+
+
+public class ProntoSocorro extends Hospital {
+    private Hospital matriz;
+    private int IdFilial;
+    private String Endereco;
+    
+    
+    
+    public void setMatriz(Hospital matriz){
+    this.matriz = matriz;
+    
+    }
+    
+    
+    
+    public Hospital getMatriz(){
+        return matriz;
+        
+    }
+ 
+    
+    
+    public void setIdFilial(int IdFilial){
+        if (!Objects.isNull(IdFilial)){
+            this.IdFilial = IdFilial;
+            
+        }
+    }    
+    
+    
+    
+        public int getIdFilial(){
+        return this.IdFilial;
+        
+        }  
+        
+        
+        
+        
+    public void setEndereco(String Endereco){
+        if (!"".equals(Endereco)){
+           this.Endereco = Endereco; 
+           
+        }
+    }    
+    
+    
+    
+        public String getEndereco(){
+        return this.Endereco;
+        
+    } 
+        
+        
+        
+        public ProntoSocorro(Hospital matriz, int idFilial){
+            this.matriz = matriz;
+            this.IdFilial = idFilial;
+        
+        }
+
+        
+     
+}        
+    
+
+
